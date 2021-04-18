5c697f0fc7709fd4839f2b288cdee06fa10c66fa
leth2 <happynsad@gmail.com>
2021-04-19 06:03:58 +0900

    localStorage 적용

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,38 +1,45 @@
 <template>
-    <div>
-        <label for="todo-input">input test</label>
-        <input id="todo-input" type="text" :value="item" @input="handleInput"/>
-        <button type="button"></button>
-    </div>
+  <div>
+    <label for="todo-input">input test</label>
+    <input id="todo-input" type="text" :value="_item" @input="handleInput" />
+    <button type="button" @click="handleClick">추가</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, getCurrentInstance, reactive, toRefs } from '@vue/composition-api'
+import {
+  defineComponent,
+  getCurrentInstance,
+  reactive,
+  toRefs,
+} from '@vue/composition-api';
 
 export default defineComponent({
-    props : {
-        item : {
-            type: String,
-            required : true
-        }
+  props: {
+    item: {
+      type: String,
+      required: true,
     },
-    setup (props) {
-        const vm = getCurrentInstance()
-        const state = reactive({
-            item : props.item
-        })
-        const handleInput = (e: InputEvent) => {
-            console.log(e)
-            const el = e.target as HTMLInputElement
-            if(el) {
-                vm.emit(el.value)
-            }
-        }
-        return {...toRefs(state)}
-    }
-})
+  },
+  setup(props) {
+    const vm = getCurrentInstance();
+    const state = reactive({
+      _item: props.item,
+    });
+    const handleInput = (e: InputEvent) => {
+      const el = e.target as HTMLInputElement;
+      if (el && vm) {
+        vm.emit('todoInput', el.value);
+      }
+    };
+    const handleClick = () => {
+      if (vm) {
+        vm.emit('addTodo');
+      }
+    };
+    return { ...toRefs(state), handleInput, handleClick };
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/todoIndex.vue
+++ src/pages/todoIndex.vue
@@ -1,15 +1,83 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <todo-input
+      :item="item"
+      @todoInput="handleTodoInput"
+      @addTodo="handleAddTodo"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import {
+  defineComponent,
+  onMounted,
+  reactive,
+  toRefs,
+} from '@vue/composition-api';
+import TodoInput from 'src/components/TodoInput.vue';
+
+interface Todo {
+  item: string;
+}
+
+function TodoTypeGuard(o: any): o is Todo[] {
+  if (o instanceof Array) {
+    return 'item' in o[0];
+  } else {
+    return false;
+  }
+}
 
 export default defineComponent({
-    setup() {
-        
-    },
-})
+  components: { TodoInput },
+  name: 'TodoIndex',
+  setup() {
+    const state = reactive({
+      item: '',
+    });
+    const handleTodoInput = (s: string) => {
+      //console.log(s);
+      state.item = s;
+    };
+    const handleAddTodo = () => {
+      //console.log('add todo' + state.item);
+      const localTodoData = localStorage.getItem('todos');
+      let todoData: Todo[];
+      if (localTodoData) {
+        const d: unknown = JSON.parse(localTodoData);
+        if (TodoTypeGuard(d)) {
+          todoData = d;
+        } else {
+          todoData = [];
+        }
+      } else {
+        todoData = [];
+      }
+      todoData.push({ item: state.item });
+
+      localStorage.setItem('todos', JSON.stringify(todoData));
+    };
+    onMounted(() => {
+      const localTodoData = localStorage.getItem('todos');
+      let todoData: Todo[];
+      if (localTodoData) {
+        const d: unknown = JSON.parse(localTodoData);
+        if (TodoTypeGuard(d)) {
+          todoData = d;
+        } else {
+          todoData = [];
+        }
+      } else {
+        todoData = [];
+      }
+      console.log(todoData);
+    });
+    return {
+      ...toRefs(state),
+      handleTodoInput,
+      handleAddTodo,
+    };
+  },
+});
 </script>
